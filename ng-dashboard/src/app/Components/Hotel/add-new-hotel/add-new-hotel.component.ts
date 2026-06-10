325d2d7272893c7f3fbd1d77d66a3bcd8590f0bb
Vaibhav <developer.netcore88@gmail.com>
2026-06-11 00:37:24 +0530

    Added input controls on add new hotel page (only UI)

--- ng-dashboard/src/app/Components/Hotel/add-new-hotel/add-new-hotel.component.ts
+++ ng-dashboard/src/app/Components/Hotel/add-new-hotel/add-new-hotel.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+import { BaseComponent } from '../../BaseComponent/base-component';
+import { BarLoadingService } from 'src/app/Elements/Dashboard/bar-loading/bar-loading.service';
+import { BarMessageService } from 'src/app/Elements/Dashboard/bar-message/bar-message.service';
 
 @Component({
   selector: 'app-add-new-hotel',
   templateUrl: './add-new-hotel.component.html',
   styleUrls: ['./add-new-hotel.component.scss']
 })
-export class AddNewHotelComponent {
+export class AddNewHotelComponent extends BaseComponent {
+  constructor(
+      barLoadingService:BarLoadingService,
+      barMessageService:BarMessageService
+   ) {
+     super(barLoadingService,barMessageService);
+  }
 
+  addLoading(t:boolean){
+    this.barLoadingService.loadingStatusEvent(t);
+  }
 }
